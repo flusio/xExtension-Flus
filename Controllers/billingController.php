053e29d0e77b1a3dfeb283303ec90e137ea79d58
Marien Fressinaud <dev@marienfressinaud.fr>
2019-08-11 11:57:05 +0200

    Display user subscription_end_at on billing page

--- Controllers/billingController.php
+++ Controllers/billingController.php
@@ -2,6 +2,17 @@
 
 class FreshExtension_billing_Controller extends FreshRSS_index_Controller {
     public function indexAction() {
+        if (!FreshRSS_Auth::hasAccess()) {
+            Minz_Error::error(403);
+        }
+
         Minz_View::prependTitle('Facturation · ');
+
+        $user_conf = FreshRSS_Context::$user_conf;
+        $this->view->today = time();
+        $this->view->subscription_end_at = $user_conf->billing['subscription_end_at'];
+        $this->view->subscription_end_at_label = timestamptodate(
+            $this->view->subscription_end_at, false
+        );
     }
 }
