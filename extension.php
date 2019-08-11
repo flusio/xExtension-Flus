cc2c3e3ef9562e4590012cb5035c41bbbf726ff3
Marien Fressinaud <dev@marienfressinaud.fr>
2019-08-11 11:57:05 +0200

    Initialize billing in configuration

--- extension.php
+++ extension.php
@@ -7,6 +7,7 @@ class FlusExtension extends Minz_Extension {
         $this->registerViews();
 
         $this->registerHook('menu_configuration_entry', array('FlusExtension', 'getMenuEntry'));
+        $this->registerHook('freshrss_init', array('FlusExtension', 'initBillingConfiguration'));
     }
 
     public static function getMenuEntry() {
@@ -19,5 +20,26 @@ class FlusExtension extends Minz_Extension {
         $label = 'Facturation';
 
         return "<li class=\"item$active_class\"><a href=\"$url\">$label</a></li>";
+    }
+
+    public static function initBillingConfiguration() {
+        $user_conf = FreshRSS_Context::$user_conf;
+        if ($user_conf && !is_array($user_conf->billing)) {
+            $user_conf->billing = array(
+                'subscription_end_at' => strtotime("+1 month"),
+                'subscription_frequency' => 'month',
+                'subscription_type' => 'card',
+            );
+            $user_conf->save();
+        }
+
+        $system_conf = FreshRSS_Context::$system_conf;
+        if ($system_conf && !is_array($system_conf->billing)) {
+            $system_conf->billing = array(
+                'month_price' => 5,
+                'year_price' => 50,
+            );
+            $system_conf->save();
+        }
     }
 }
