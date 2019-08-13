d6a8b13d7a0775adc0eb81aab41d45640d363c50
Marien Fressinaud <dev@marienfressinaud.fr>
2019-08-13 17:16:42 +0200

    Remember payment options across renewals

--- Controllers/billingController.php
+++ Controllers/billingController.php
@@ -28,17 +28,26 @@ class FreshExtension_billing_Controller extends FreshRSS_index_Controller {
         Minz_View::prependTitle('Facturation · ');
 
         $system_conf = FreshRSS_Context::$system_conf;
+        $user_conf = FreshRSS_Context::$user_conf;
+        $billing = $user_conf->billing;
 
         $this->view->paypal_illustration = $this->extension->getFileUrl('paypal.jpg', 'jpg');
         $this->view->cb_illustration = $this->extension->getFileUrl('cb.jpg', 'jpg');
         $this->view->month_price = $system_conf->billing['month_price'];
         $this->view->year_price = $system_conf->billing['year_price'];
+        $this->view->subscription_frequency = $billing['subscription_frequency'];
+        $this->view->subscription_type = $billing['subscription_type'];
 
         if (Minz_Request::isPost()) {
             // @todo this should be handled on payment service callback
             $frequency = Minz_Request::param('frequency', 'month');
-            $user_conf = FreshRSS_Context::$user_conf;
-            $billing = $user_conf->billing;
+            if ($frequency !== 'month' && $frequency !== 'year') {
+                $frequency = 'month';
+            }
+            $payment_type = Minz_Request::param('payment-type', 'card');
+            if ($payment_type !== 'card' && $payment_type !== 'paypal') {
+                $payment_type = 'card';
+            }
 
             if ($frequency === 'year') {
                 $interval = '1 year';
@@ -59,6 +68,9 @@ class FreshExtension_billing_Controller extends FreshRSS_index_Controller {
                 );
                 $billing['subscription_end_at'] = $subscription_end_at->getTimestamp();
             }
+
+            $billing['subscription_frequency'] = $frequency;
+            $billing['subscription_type'] = $payment_type;
 
             $user_conf->billing = $billing;
             if ($user_conf->save()) {
