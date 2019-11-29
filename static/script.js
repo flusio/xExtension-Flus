46673cae97c24f209a10e16fc37354c4c8582a21
Marien Fressinaud <dev@marienfressinaud.fr>
2019-11-29 21:23:16 +0100

    add: Warn user if he tries to login with email
    This is a temporary fix in the extension. We should either have it in
    the core, or even better: allow to login with email!

--- static/script.js
+++ static/script.js
@@ -1,4 +1,36 @@
 (function () {
+    // Add a warning on auth if user try to login with an email
+    var url = new URL(window.location.href);
+    var controllerName = url.searchParams.get('c');
+    var actionName = url.searchParams.get('a');
+    if (controllerName === 'auth' && actionName === 'login') {
+        // Create a paragraph to explain to use username and not email address,
+        // hidden by default.
+        var useUsernameParagraph = document.createElement('p');
+        var useUsernameContent = document.createTextNode(
+            'Attention à bien saisir votre nom d’utilisateur et non votre adresse email !'
+        );
+        useUsernameParagraph.appendChild(useUsernameContent);
+        useUsernameParagraph.id = 'use-username-info';
+        useUsernameParagraph.className = 'alert alert-warn';
+        useUsernameParagraph.style.display = 'none';
+
+        // Insert the paragraph after the username input
+        var usernameInput = document.getElementById('username');
+        usernameInput.parentNode.insertBefore(useUsernameParagraph, usernameInput.nextSibling);
+
+        // Listen on username input change to check if user insert a '@'. If it's
+        // the case, display the paragraph.
+        usernameInput.addEventListener('keyup', function() {
+            if (usernameInput.value.indexOf('@') !== -1) {
+                useUsernameParagraph.style.display = 'block';
+            } else {
+                useUsernameParagraph.style.display = 'none';
+            }
+        });
+    }
+
+    // Manage the billing renew form
     var monthPrice = document.getElementById('renew-amount-month');
     var yearPrice = document.getElementById('renew-amount-year');
     var radios = document.querySelectorAll('input[name="frequency"]');
