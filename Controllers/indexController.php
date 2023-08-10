86a1b56f6c9793a75c2d7e5f077034581253f28b
Marien Fressinaud <dev@marienfressinaud.fr>
2023-08-10 14:07:53 +0200

    tec: Remove unused app icons

--- Controllers/indexController.php
+++ Controllers/indexController.php
@@ -19,19 +19,6 @@ class FreshExtension_index_Controller extends FreshRSS_index_Controller {
             Minz_View::appendStyle($this->extension->getFileUrl('home.css', 'css'));
             $this->view->registration_opened = !max_registrations_reached();
             $this->view->illustration_url = $this->extension->getFileUrl('screenshot.png', 'png');
-            $this->view->app_icons = [
-                'easyrss' => $this->extension->getFileUrl('app-icons/easyrss.png', 'png'),
-                'feedme' => $this->extension->getFileUrl('app-icons/feedme.png', 'png'),
-                'readably' => $this->extension->getFileUrl('app-icons/readably.png', 'png'),
-                'reeder' => $this->extension->getFileUrl('app-icons/reeder.png', 'png'),
-                'fiery-feeds' => $this->extension->getFileUrl('app-icons/fiery-feeds.png', 'png'),
-                'unread' => $this->extension->getFileUrl('app-icons/unread.png', 'png'),
-                'vienna' => $this->extension->getFileUrl('app-icons/vienna.png', 'png'),
-                'reeder-ios' => $this->extension->getFileUrl('app-icons/reeder-ios.png', 'png'),
-                'reeder-macos' => $this->extension->getFileUrl('app-icons/reeder-macos.png', 'png'),
-                'readkit' => $this->extension->getFileUrl('app-icons/readkit.png', 'png'),
-                'feedreader' => $this->extension->getFileUrl('app-icons/feedreader.png', 'png'),
-            ];
             $system_conf = FreshRSS_Context::$system_conf;
             $this->view->max_registrations = $system_conf->limits['max_registrations'];
             Minz_View::prependTitle('Suivez l’actualité qui vous intéresse · ');
