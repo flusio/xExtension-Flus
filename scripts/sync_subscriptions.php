15eed32c2a8e7e16e4076967c90f8d51f6b67866
Marien Fressinaud <dev@marienfressinaud.fr>
2023-09-06 15:11:29 +0200

    tec: Make sure that overdue users are inactive
    This is to avoid to refresh the feeds of these users.

--- scripts/sync_subscriptions.php
+++ scripts/sync_subscriptions.php
@@ -26,9 +26,13 @@ $flus_api_host = FreshRSS_Context::$system_conf->billing['flus_api_host'];
 $flus_private_key = FreshRSS_Context::$system_conf->billing['flus_private_key'];
 $subscriptions_service = new \Flus\services\Subscriptions($flus_api_host, $flus_private_key);
 
+$limits = FreshRSS_Context::$system_conf->limits;
+$min_last_activity = time() - $limits['max_inactivity'];
+
 // First, make sure all users have a subscription account.
 $usernames = listUsers();
 $account_ids_to_user_confs = [];
+$account_ids_to_usernames = [];
 foreach ($usernames as $username) {
     $user_conf = get_user_configuration($username);
     if (!$user_conf) {
@@ -62,8 +66,10 @@ foreach ($usernames as $username) {
         $user_conf->save();
 
         $account_ids_to_user_confs[$account['id']] = $user_conf;
+        $account_ids_to_usernames[$account['id']] = $username;
     } elseif (!$no_account) {
         $account_ids_to_user_confs[$subscription['account_id']] = $user_conf;
+        $account_ids_to_usernames[$subscription['account_id']] = $username;
     }
 }
 
@@ -86,5 +92,21 @@ foreach ($result as $account_id => $expired_at) {
         $subscription['expired_at'] = $expired_at;
         $user_conf->subscription = $subscription;
         $user_conf->save();
+    }
+
+    // Make the user inactive if account is overdue
+    $today = new \DateTime();
+    $expired_at_date = date_create_from_format('Y-m-d H:i:sP', $expired_at);
+    if ($expired_at_date === false) {
+        \Minz\Log::warning("Subscription account {$account_id} has invalid expiration date ({$expired_at}).");
+        continue;
+    }
+
+    $username = $account_ids_to_usernames[$account_id];
+    $free_account = $expired_at_date->getTimestamp() === 0;
+    $subscription_is_overdue = $today >= $expired_at_date;
+    $is_inactive = FreshRSS_UserDAO::mtime($username) < $min_last_activity;
+    if (!$free_account && $subscription_is_overdue && !$is_inactive) {
+        touch(USERS_PATH . '/' . $username . '/config.php', $min_last_activity);
     }
 }
